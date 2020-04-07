cec98388d456a67fc727940fed07aa6d0a9107d9
sfongue <sfongue@gmail.com>
2020-04-07 14:19:29 +0200

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData = [
     {name:'Mark'},
@@ -21,6 +24,27 @@ app.get('/users/:id',function(req,res){
         message: 'got one user',
         user: req.params.id
     })
+})
+
+app.post('/login',function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    const mockUsername = "Babar";
+    const mockPassword = "elephant";
+
+    if (username === mockUsername && password === mockPassword){
+        res.json({
+            success : true,
+            message: 'password and username match',
+            token: 'encrypted token'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'wrong password or username'
+        })
+    }
 })
 
 app.listen(8000,function(){
